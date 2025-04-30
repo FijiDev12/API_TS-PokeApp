0dc6ab3357cef557e28dcad02481f7f2204ce0b9
Sid <izidrobernadas7@gmail.com>
2025-04-30 13:04:51 +0800

    fix error regex

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,6 @@ import https from 'https';
 
 const PORT = process.env.PORT || 8502;
 
-app.get('*', (req, res, next) => {
-	res.status(404).json({
-		"code": 404,
-		"message": "Invalid endpoint."
-	});
-
-	return;
-});
-
 const sslOptions = {
     key: fs.readFileSync('/etc/letsencrypt/live/api-psp-ext.festivalcasino.com/fullchain.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/api-psp-ext.festivalcasino.com/privkey.pem')
