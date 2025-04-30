c6744378a0d037e84801cb8995cbe9177fd48291
Sid <izidrobernadas7@gmail.com>
2025-04-30 13:00:11 +0800

    update SSL

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
 import app from './app';
+import fs from 'fs';
+import https from 'https';
 
 const PORT = process.env.PORT || 8502;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.get('*', (req, res, next) => {
+	res.status(404).json({
+		"code": 404,
+		"message": "Invalid endpoint."
+	});
+
+	return;
+});
+
+const sslOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/api-psp-ext.festivalcasino.com/fullchain.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api-psp-ext.festivalcasino.com/privkey.pem')
+}
+
+https.createServer(sslOptions, app).listen(443, () => {
+    console.log(`🚀 Server running on https://0.0.0.0:${PORT}`);
+});
