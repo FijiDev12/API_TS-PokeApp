4587046151730e4acf7c52dd0d817038fd29f5f4
Sid <izidrobernadas7@gmail.com>
2025-04-30 13:11:08 +0800

    update ssl options

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import https from 'https';
 const PORT = process.env.PORT || 8502;
 
 const sslOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/api-psp-ext.festivalcasino.com/fullchain.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/api-psp-ext.festivalcasino.com/privkey.pem')
+    key: fs.readFileSync('/etc/letsencrypt/live/api-psp-ext.festivalcasino.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api-psp-ext.festivalcasino.com/fullchain.pem')
 }
 
 https.createServer(sslOptions, app).listen(443, () => {
